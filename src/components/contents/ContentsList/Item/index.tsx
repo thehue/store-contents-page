862f34997aaec3dd8111d2b8e302c914dbc3e8fb
thehue <gouk32@naver.com>
2022-07-04 00:31:02 +0900

    Feat: ContentsList 컴포넌트 추가
    디바이스 가로 길이에 따라 검색 결과 row당 column수 변경

---
+++ src/components/contents/ContentsList/Item/index.tsx
@@ -0,0 +1,104 @@
+import styled from 'styled-components';
+import { PricingOption } from '..';
+
+type ItemProps = {
+  imagePath: string;
+  title: string;
+  creator: string;
+  price: number;
+  pricingOption: PricingOption;
+};
+
+export default function Item({
+  imagePath,
+  title,
+  creator,
+  price,
+  pricingOption,
+}: ItemProps) {
+  const formatPricingOption = {
+    0: `$${price.toLocaleString()}`,
+    1: 'FREE',
+    2: 'View Only',
+  };
+
+  return (
+    <ItemWrapper>
+      <Inner>
+        <Thumbnail imagePath={imagePath} />
+        <ItemInfoWrapper>
+          <ItemName>
+            <Text>{title}</Text>
+          </ItemName>
+          <ItemCreatorName>
+            <Text>{creator}</Text>
+          </ItemCreatorName>
+          <Text>{formatPricingOption[pricingOption]}</Text>
+        </ItemInfoWrapper>
+      </Inner>
+    </ItemWrapper>
+  );
+}
+
+const ItemWrapper = styled.div`
+  width: 25%;
+  padding-right: 12px;
+  padding-left: 12px;
+  box-sizing: border-box;
+
+  @media screen and (max-width: 1200px) {
+    width: 33.33%;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 50%;
+  }
+
+  @media screen and (max-width: 480px) {
+    width: 100%;
+  }
+`;
+
+const Inner = styled.div`
+  padding-bottom: 40px;
+`;
+
+const Thumbnail = styled.div<{ imagePath: string }>`
+  position: relative;
+  padding-top: 133%;
+  border-radius: 3px;
+  background-color: white;
+  background-image: url(${({ imagePath }) => imagePath});
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  cursor: pointer;
+`;
+
+const ItemInfoWrapper = styled.div`
+  margin-top: 8px;
+`;
+
+const Text = styled.span`
+  font-size: 1.2rem;
+  line-height: inherit;
+  font-weight: inherit;
+`;
+
+const ItemName = styled.div`
+  margin-bottom: 2px;
+  word-break: break-all;
+  line-height: 16px;
+  font-weight: 600;
+`;
+
+const ItemCreatorName = styled.div`
+  display: inline-block;
+  line-height: 18px;
+  font-weight: 300;
+
+  &:after {
+    content: '|';
+    padding: 0px 4px;
+  }
+`;
--- src/components/contents/ContentsList/index.tsx
+++ src/components/contents/ContentsList/index.tsx
@@ -1,3 +1,92 @@
+import styled from 'styled-components';
+import Item from './Item';
+
+export const PricingOption = {
+  PAID: 0,
+  FREE: 1,
+  VIEW_ONLY: 2,
+} as const;
+
+export type PricingOption = typeof PricingOption[keyof typeof PricingOption];
+
+const sampleData = [
+  {
+    id: 'content-001',
+    creator: 'Adam',
+    title: 'Yellow green coat',
+    pricingOption: 0,
+    imagePath:
+      'https://storageaccountcloseb093.blob.core.windows.net/frontend-recruiting-sample/thumbnails/FE%20Test%20Material/thumbnail_1.jpeg',
+    price: 50,
+  },
+  {
+    id: 'content-002',
+    creator: 'Benny',
+    title: 'Brown Anorak',
+    pricingOption: 1,
+    imagePath:
+      'https://storageaccountcloseb093.blob.core.windows.net/frontend-recruiting-sample/thumbnails/FE%20Test%20Material/thumbnail_2.png',
+    price: 30,
+  },
+  {
+    id: 'content-003',
+    creator: 'Catlin',
+    title: 'Block shape mini bag',
+    pricingOption: 2,
+    imagePath:
+      'https://storageaccountcloseb093.blob.core.windows.net/frontend-recruiting-sample/thumbnails/FE%20Test%20Material/thumbnail_3.jpeg',
+    price: 15,
+  },
+  {
+    id: 'content-004',
+    creator: 'Dan',
+    title: 'Tartan mini dress',
+    pricingOption: 1,
+    imagePath:
+      'https://storageaccountcloseb093.blob.core.windows.net/frontend-recruiting-sample/thumbnails/FE%20Test%20Material/thumbnail_4.png',
+    price: 300,
+  },
+  {
+    id: 'content-005',
+    creator: 'Emily',
+    title: 'Pink training suit',
+    pricingOption: 2,
+    imagePath:
+      'https://storageaccountcloseb093.blob.core.windows.net/frontend-recruiting-sample/thumbnails/FE%20Test%20Material/thumbnail_5.png',
+    price: 200.5,
+  },
+] as {
+  id: string;
+  creator: string;
+  title: string;
+  pricingOption: PricingOption;
+  imagePath: string;
+  price: number;
+}[];
+
 export default function ContentsList() {
-  return <div>ContentsList</div>;
+  return (
+    <ItemList>
+      {sampleData.map(
+        ({ id, imagePath, title, creator, price, pricingOption }) => (
+          <Item
+            key={id}
+            imagePath={imagePath}
+            title={title}
+            creator={creator}
+            price={price}
+            pricingOption={pricingOption}
+          />
+        ),
+      )}
+    </ItemList>
+  );
 }
+
+const ItemList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  margin: 0 -12px;
+  margin-top: 60px;
+`;
