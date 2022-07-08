3f61753285f25b7053f643f84cdd194a2e9004b6
thehue <gouk32@naver.com>
2022-07-08 17:26:58 +0900

    chore: router basename 재설정

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ const store = configureStore({
 if (container) {
   const root = createRoot(container);
   root.render(
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/store-contents-page/">
       <Provider store={store}>
         <App />
       </Provider>
