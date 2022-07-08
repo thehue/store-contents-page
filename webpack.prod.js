a1248547a6036842a1600d5a4b68e360716508e7
thehue <gouk32@naver.com>
2022-07-08 15:40:48 +0900

    chore: 번들 분할 작업

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,16 @@
 const { merge } = require('webpack-merge');
+
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'cheap-module-source-map',
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+    runtimeChunk: {
+      name: 'runtime',
+    },
+  },
 });
